1363db97a8a4e6f72a5ee4acea7b1a2e0aa97d06
Erin Dustin <erincdustin@gmail.com>
2019-05-28 14:55:50 -0500

    added error handling

--- src/App.js
+++ src/App.js
@@ -6,107 +6,6 @@ import BookList from './BookList';
 
 const API_KEY = 'AIzaSyDQ1v1nWOS3VQALosV4Pn2JPhcjMibUShg';
 
-const data = [{
-  "kind": "books#volume",
-  "id": "BL7HpqQvU34C",
-  "etag": "lV/MG6BSSrs",
-  "selfLink": "https://www.googleapis.com/books/v1/volumes/BL7HpqQvU34C",
-  "volumeInfo": {
-      "title": "Apple Inc",
-      "authors": [
-          "Jason D. O'Grady"
-      ],
-      "publisher": "ABC-CLIO",
-      "publishedDate": "2009",
-      "description": "Assesses Apple's impact on society, technology, processes, and methods; shows how Apple beat the competition in selected markets; details financial results over the years; and, predicts Apple's future prospects and successes. In addition, the author offers special features that include a look at the colourful people associated with Apple, interesting trivia, an Apple time line, a focus on products, and where the company is headed",
-      "industryIdentifiers": [
-          {
-              "type": "ISBN_13",
-              "identifier": "9780313362446"
-          },
-          {
-              "type": "ISBN_10",
-              "identifier": "0313362440"
-          }
-      ],
-      "readingModes": {
-          "text": true,
-          "image": true
-      },
-      "pageCount": 185,
-      "printType": "BOOK",
-      "categories": [
-          "Business & Economics"
-      ],
-      "averageRating": 4,
-      "ratingsCount": 3,
-      "maturityRating": "NOT_MATURE",
-      "allowAnonLogging": true,
-      "contentVersion": "0.0.1.0.preview.3",
-      "panelizationSummary": {
-          "containsEpubBubbles": false,
-          "containsImageBubbles": false
-      },
-      "imageLinks": {
-          "smallThumbnail": "http://books.google.com/books/content?id=BL7HpqQvU34C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-          "thumbnail": "http://books.google.com/books/content?id=BL7HpqQvU34C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-      },
-      "language": "en",
-      "previewLink": "http://books.google.com/books?id=BL7HpqQvU34C&printsec=frontcover&dq=apple&hl=&cd=4&source=gbs_api",
-      "infoLink": "https://play.google.com/store/books/details?id=BL7HpqQvU34C&source=gbs_api",
-      "canonicalVolumeLink": "https://market.android.com/details?id=book-BL7HpqQvU34C"
-  },
-  "saleInfo": {
-      "country": "US",
-      "saleability": "FOR_SALE",
-      "isEbook": true,
-      "listPrice": {
-          "amount": 45,
-          "currencyCode": "USD"
-      },
-      "retailPrice": {
-          "amount": 36,
-          "currencyCode": "USD"
-      },
-      "buyLink": "https://play.google.com/store/books/details?id=BL7HpqQvU34C&rdid=book-BL7HpqQvU34C&rdot=1&source=gbs_api",
-      "offers": [
-          {
-              "finskyOfferType": 1,
-              "listPrice": {
-                  "amountInMicros": 45000000,
-                  "currencyCode": "USD"
-              },
-              "retailPrice": {
-                  "amountInMicros": 36000000,
-                  "currencyCode": "USD"
-              },
-              "giftable": true
-          }
-      ]
-  },
-  "accessInfo": {
-      "country": "US",
-      "viewability": "PARTIAL",
-      "embeddable": true,
-      "publicDomain": false,
-      "textToSpeechPermission": "ALLOWED",
-      "epub": {
-          "isAvailable": true,
-          "acsTokenLink": "http://books.google.com/books/download/Apple_Inc-sample-epub.acsm?id=BL7HpqQvU34C&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
-      },
-      "pdf": {
-          "isAvailable": true,
-          "acsTokenLink": "http://books.google.com/books/download/Apple_Inc-sample-pdf.acsm?id=BL7HpqQvU34C&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
-      },
-      "webReaderLink": "http://play.google.com/books/reader?id=BL7HpqQvU34C&hl=&printsec=frontcover&source=gbs_api",
-      "accessViewStatus": "SAMPLE",
-      "quoteSharingAllowed": false
-  },
-  "searchInfo": {
-      "textSnippet": "Discusses the founding and success of Apple, examining its historical context, strategies and innovations, influence on society, various technologies, processes, and methods, and competition in various markets, with trivia and a look at the ..."
-  }
-}];
-
 class App extends React.Component{
 
   state = {
@@ -151,13 +50,20 @@ class App extends React.Component{
     url += `&key=${API_KEY}`;
     console.log(url);
     fetch(url)
-      .then(res => res.json())
+      .then(res => res.ok ? res.json() : Promise.reject('something went wrong'))
       .then(res => {
         console.log(res);
         this.setState({
           books: res.items
         })
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          error: err
+        })
       });
+      
   }
 
   updatePrintType = (type) => {
@@ -174,6 +80,18 @@ class App extends React.Component{
 
   render() {
     // console.log(this.state);
+    if (this.state.error) {
+      return (
+        <div className="App">
+        <header className="App-header">
+          <Header updateSearchTerm={this.updateSearchTerm} updatePrintType={this.updatePrintType} updateFilterType={this.updateFilterType}/>
+        </header>
+        <main>
+        <div>{this.state.error}</div>
+        </main>
+      </div>
+      )
+    }
 
     return (
       <div className="App">
--- src/BookList.js
+++ src/BookList.js
@@ -3,8 +3,7 @@ import Book from './Book';
 
 function BookList(props) {
   const mappedBooks = props.books.map(item => {
-    
-    return <Book key={item.id} title={item.volumeInfo.title} authors={item.volumeInfo.authors} price={item.saleInfo.listPrice.amount} description={item.volumeInfo.description} img={item.volumeInfo.imageLinks.thumbnail} />
+    return <Book key={item.id} title={item.volumeInfo.title} authors={item.volumeInfo.authors ? item.volumeInfo.authors : ''} price={item.saleInfo.saleability === 'FOR_SALE' ? item.saleInfo.listPrice.amount : 'FREE'} description={item.volumeInfo.description ? item.volumeInfo.description : ''} img={item.volumeInfo.imageLinks.thumbnail ? item.volumeInfo.imageLinks.thumbnail : ''} />
   });
 
   return (
